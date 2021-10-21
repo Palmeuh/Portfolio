cf40b4d9bc861936272251f2a5726bbc2fb612e3
Fredrik Palmé <fredrik.palme@hotmail.com>
2021-10-21 11:03:44 +0200

    Added bootstrap

--- wp-content/themes/portfoliotheme/functions.php
+++ wp-content/themes/portfoliotheme/functions.php
@@ -4,20 +4,19 @@
 function load_css()
 {
 
-    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', [], 1, 'all');
+    wp_register_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', [], 1, 'all');
     wp_enqueue_style('bootstrap');
 
-    wp_register_style('main', get_template_directory_uri() . '/css/main.css', [], 1, 'all');
+    wp_register_style('main', get_template_directory_uri() . '/style.css', [], 1, 'all');
     wp_enqueue_style('main');
 
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
 function load_js()
-{
-    wp_enqueue_script('jquery');
+{   
 
-    wp_register_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', ['jquery'], 1, true);
+    wp_register_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.min.js', [], 1, true);
     wp_enqueue_script('bootstrap');
 }
 add_action('wp_enqueue_scripts', 'load_js');
