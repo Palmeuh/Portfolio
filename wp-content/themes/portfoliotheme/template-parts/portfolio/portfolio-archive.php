6f70eef505259532515499958ab3579c0d94dc6b
Fredrik Palmé <70195787+Palmeuh@users.noreply.github.com>
2021-10-27 15:16:13 +0200

    working comments (#5)

--- wp-content/themes/portfoliotheme/template-parts/portfolio/portfolio-archive.php
+++ wp-content/themes/portfoliotheme/template-parts/portfolio/portfolio-archive.php
@@ -1,12 +1,24 @@
 
 <div class="col ">
-    <div class="card h-100 w-75 bg-light mx-auto text-center">
+    <div class="card h-100 w-75 bg-light mx-auto text-center shadow-lg">
       <img src="<?php the_post_thumbnail_url() ?>" class="card-img-top img-fluid" alt="...">
-      <div class="card-body bg-dark d-flex flex-column">
+      <div class="card-body bg-light d-flex flex-column">
+          
         <h5 class="card-title"><?php the_title(); ?></h5>
+        <div class="text-muted">
+        <div class="meta mb-3">
+            <span class="date">Published: <?php the_date(); ?></span>            
+
+            <?php 
+                the_tags('<span class="tag"><i class="fa fa-tag"></i> ', '</span><span class="tag"><i class="fa fa-tag"></i> ', '</span>');
+            ?>            
+            
+            <span class="comment"><a href="#comments"><i class='fa fa-comment'></i> <?php comments_number(); ?></a></span>
+        </div>  
+        </div>
         <p class="card-text"><?php the_excerpt() ?></p>
-        <a href="<?php the_permalink(); ?>" class="btn btn-outline-light mt-auto">Learn more</a>
-      </div>
+        <a href="<?php the_permalink(); ?>" class="btn btn-dark mt-auto">Learn more</a>
+      </div>      
     </div>
 </div>
 
